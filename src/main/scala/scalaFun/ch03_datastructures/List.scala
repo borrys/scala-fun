fe8a5179a860b8290fb33329b9221b4e769f3bde
borrys <andrzej.kimel@gmail.com>
2017-02-09 19:26:58 +0100

    custom List implementation

---
+++ src/main/scala/scalaFun/ch03_datastructures/List.scala
@@ -0,0 +1,85 @@
+package scalaFun.ch03_datastructures
+
+import scala.annotation.tailrec
+
+sealed trait List[+A]
+
+case object Nil extends List[Nothing]
+
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h, t) => Cons(h, append(t, a2))
+    }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def tail[A](l: List[A]): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(_, t) => t
+    }
+
+  def setHead[A](l: List[A], h: A): List[A] =
+    l match {
+      case Nil => sys.error("setHead on empty list")
+      case Cons(_, t) => Cons(h, t)
+    }
+
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => sys.error("not enough elements in list")
+      case Cons(_, t) => drop(t, n - 1)
+    }
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(a, t) if f(a) => dropWhile(t, f)
+      case _ => l
+    }
+
+  def init[A](l: List[A]): List[A] = {
+    @tailrec
+    def dropLast(xs: List[A], acc:List[A] =List()): List[A] = {
+      xs match {
+        case Nil => sys.error("init on empty list")
+        case Cons(_, Nil) => acc
+        case Cons(a, t) => dropLast(t, Cons(a, acc))
+      }
+    }
+
+    @tailrec
+    def reverse(xs: List[A], acc:List[A]):List[A] = {
+      xs match {
+        case Nil => acc
+        case Cons(a, t) => reverse(t, Cons(a, acc))
+      }
+    }
+
+    val allButLast = dropLast(l)
+    reverse(allButLast, List())
+  }
+}
---
+++ src/test/scala/scalaFun/ch03_datastructures/List$Test.scala
@@ -0,0 +1,115 @@
+package scalaFun.ch03_datastructures
+
+import org.scalatest.WordSpec
+
+import scalaFun.ch03_datastructures.List._
+
+class List$Test extends WordSpec {
+  "List.tail" when {
+    "given empty list" should {
+      "return empty list" in {
+        assert(tail(List()) == List())
+      }
+    }
+    "given non-empty list" should {
+      "return all elements of the list but first" in {
+        assert(tail(List(1, 2, 3, 4)) == List(2, 3, 4))
+      }
+    }
+  }
+
+  "List.setHead" when {
+    "given empty list" should {
+      "raise an exception for empty list" in {
+        assertThrows[RuntimeException] {
+          setHead(List(), 5)
+        }
+      }
+    }
+
+    "given non-empty list" should {
+      "return list with first element swapped" in {
+        assert(setHead(List("a", "b", "c"), "zzz") == List("zzz", "b", "c"))
+      }
+    }
+  }
+
+
+  "List.drop" when {
+    "given empty list" should {
+      "raise an exception for empty list" in {
+        assertThrows[RuntimeException] {
+          drop(List(), 1)
+        }
+      }
+    }
+
+    "given non-empty list and number smaller than list length" should {
+      "return list with removed initial elements" in {
+        assert(drop(List("a", "b", "c", "d"), 2) == List("c", "d"))
+      }
+    }
+
+    "given non-empty list and negative number" should {
+      "return the whole list" in {
+        assert(drop(List(1, 2, 3, 4), -5) == List(1, 2, 3, 4))
+      }
+    }
+
+    "given empty list and number greater than list length" should {
+      "raise an exception" in {
+        assertThrows[RuntimeException] {
+          drop(List(1, 2, 3), 150)
+        }
+      }
+    }
+  }
+
+  "List.dropWhile" when {
+    "given empty list" should {
+      "return empty list" in {
+        assert(dropWhile(List(), (a: Int) => a > 5) == List())
+      }
+    }
+
+    "given non-empty list and condition not fulfilled by any element" should {
+      "return the whole list" in {
+        assert(dropWhile(List(1, 2, 3, 4), (a: Int) => a > 100) == List(1, 2, 3, 4))
+      }
+    }
+
+    "given non-empty list and condition fulfilled by initial elements" should {
+      "return list without those initial elements" in {
+        assert(dropWhile(List(1, 2, 3, 4), (a: Int) => a < 3) == List(3, 4))
+      }
+    }
+
+    "given non-empty list and condition fulfilled by elements in the middle" should {
+      "return the whole list" in {
+        assert(dropWhile(List(1, 200, 300, 4), (a: Int) => a > 100) == List(1, 200, 300, 4))
+      }
+    }
+  }
+
+  "List.init" when {
+    "given empty list" should {
+      "throw exception" in {
+        assertThrows[RuntimeException] {
+          init(List())
+        }
+      }
+    }
+
+    "given list with single element" should {
+      "return empty list" in {
+        assert(init(List("a")) == List())
+      }
+    }
+
+    "given list with many elements" should {
+      "return list of all elements but last" in {
+        assert(init(List(1, 2, 3, 4, 5, 6)) == List(1, 2, 3, 4, 5))
+      }
+    }
+  }
+}
