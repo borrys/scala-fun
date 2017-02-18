8731cbfbf57178f12db12810966e0e0f541eca62
borrys <andrzej.kimel@gmail.com>
2017-02-18 18:46:43 +0100

    Stream exercises base

---
+++ src/main/scala/scalaFun/ch05_laziness/Stream.scala
@@ -0,0 +1,56 @@
+package scalaFun.ch05_laziness
+
+trait Stream[+A] {
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case Cons(h, t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b)
+
+  @annotation.tailrec
+  final def find(f: A => Boolean): Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
+  }
+
+  def take(n: Int): Stream[A] = ???
+
+  def drop(n: Int): Stream[A] = ???
+
+  def takeWhile(p: A => Boolean): Stream[A] = ???
+
+  def forAll(p: A => Boolean): Boolean = ???
+
+  def headOption: Option[A] = ???
+
+
+  def startsWith[B](s: Stream[B]): Boolean = ???
+}
+
+case object Empty extends Stream[Nothing]
+
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+
+  val ones: Stream[Int] = Stream.cons(1, ones)
+
+  def from(n: Int): Stream[Int] = ???
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = ???
+}
